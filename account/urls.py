78712cc453a21fe40fc6c666272925bc2ba1ed52
Axel Kolo <axelkolo@Axels-MacBook-Pro.local>
2021-07-13 23:54:04 +0530

    user registration

--- account/urls.py
+++ account/urls.py
@@ -28,4 +28,5 @@ path('reset/done/',
      auth_views.PasswordResetCompleteView.as_view(),
      name='password_reset_complete'),
 path('', include('django.contrib.auth.urls')),
+path('register/', views.register, name='register'),
 ]
