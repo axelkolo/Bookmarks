1b39dbe86d8b043f3b11bc3db259cc584decd263
Axel Kolo <axelkolo@Axels-MacBook-Pro.local>
2021-07-19 00:07:36 +0530

    added a success message when the user successfully updates their profile, or an error message instead if the forms contain an invalid data

--- account/views.py
+++ account/views.py
@@ -5,6 +5,7 @@ from django.shortcuts import render
 from django.contrib.auth import authenticate, login
 from .forms import LoginForm
 from django.contrib.auth.decorators import login_required
+from django.contrib import messages 
 @login_required
 
 def user_login(request):
@@ -60,21 +61,32 @@ def register(request):
 
 def edit(request):
     if request.method == 'POST':
+
         user_form = UserEditForm(instance=request.user,
                                  data=request.POST)
         profile_form = ProfileEditForm(
                                     instance=request.user.profile,
                                     data=request.POST,
                                     files=request.FILES)
+        
+        user_form = UserEditForm(instance=request.user)
+        profile_form = ProfileEditForm(instance=request.user.profile)
+
         if user_form.is_valid() and profile_form.is_valid():
             user_form.save()
             profile_form.save()
+            messages.success(request, 'Profile updated '\
+                                        'successfully')
+        else:
+            messages.error(request, 'Error updating your profile')
     else:
         user_form = UserEditForm(instance=request.user)
         profile_form = ProfileEditForm(instance=request.user.profile)
+    
+
     return render(request,
                   'account/edit.html',
-                  {'user_form': user_form,
-                   'profile_form': profile_form})                                  
+                 {'user_form': user_form,
+                  'profile_form': profile_form})                                  
 
 # Create your views here.
