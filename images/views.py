1d6ac931b0c0b4f5b94b83c6cdfba7fb757762e2
Axel Kolo <gelyn.mandossi@gmail.com>
2021-07-26 17:52:20 +0100

    adding AJAX actions with jQuery

--- images/views.py
+++ images/views.py
@@ -1,3 +1,5 @@
+from django.http import JsonResponse
+from django.views.decorators.http import require_POST
 from django.shortcuts import get_list_or_404
 from .models import Image
 from django.shortcuts import render, redirect
@@ -5,6 +7,8 @@ from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 from .forms import ImageCreateForm
 @login_required
+@login_POST
+
 def image_create(request):
     if request.method == 'POST':
         # form is sent
@@ -33,5 +37,18 @@ def image_detail(request, id, slug):
                   'images/image/detail.html',
                   {'section': 'images',
                    'image': image})                  
-
+def image_like(request):
+    image_id = request.POST.get('id')
+    action = request.POST.get('action')
+    if image_id and action:
+        try:
+            image = Image.objects.get(id=image_id)
+            if action == 'like':
+                image.users_like.add(request.user)
+            else:
+                image.users_like.remove(request.user)
+            return JsonResponse({'status':'ok'})
+        except:
+            pass
+    return JsonResponse({'status':'error'})
 # Create your views here.
